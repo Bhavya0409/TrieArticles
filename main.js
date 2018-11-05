3820e30f18c3cf1daeefb1ba1963ab5e0d20e6b0
Bhavya Shah <bhavyashah@Bhavyas-MBP.home>
2018-11-05 14:06:57 -0500

    recreated the tries data structure to be more in line with the data that is necessary

--- main.js
+++ main.js
@@ -2,26 +2,27 @@ const Promise = require('bluebird');
 const fs = Promise.promisifyAll(require('fs'));
 const rl = require('readline');
 
-const Trie = require('./tries');
+const Trie = require('./newTries');
 const i = rl.createInterface({input: process.stdin, output: process.stdout, terminal: true});
 
 const trie = new Trie();
 
 fs.readFileAsync('companies.dat', 'utf-8').then(data => {
-  const companies = data.split('\n');
-  const companiesAndNicknames = companies.map((company) => {
-    return company.split('\t');
-  })
-  console.log(companiesAndNicknames);
+	const companies = data.split('\n');
+	const companiesAndNicknames = companies.map((company) => {
+		return company.split('\t');
+	})
+	console.log(companiesAndNicknames);
 
-  i.question('Enter a news article\n', newsArticle => {
-    console.log('you entered', newsArticle);
-    end();
-  });
+	i.question('Enter a news article\n', newsArticle => {
+		console.log('you entered', newsArticle);
+		const words = newsArticle.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+		end();
+	});
 });
 
 function end() {
-  console.log('exiting...');
-  i.close();
-  process.stdin.destroy();
+	console.log('exiting...');
+	i.close();
+	process.stdin.destroy();
 }
