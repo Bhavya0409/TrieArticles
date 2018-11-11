1235cc21ab1d47717df5f8177fefd3dd67830845
eawwal <eakrumawwal@gmail.com>
2018-11-11 12:48:28 -0500

    some progress, still figuring out word separation in tries

--- main.js
+++ main.js
@@ -1,46 +1,127 @@
-const Promise = require('bluebird');
-const fs = Promise.promisifyAll(require('fs'));
-const rl = Promise.promisifyAll(require('readline'));
+const Promise = require("bluebird");
+const fs = Promise.promisifyAll(require("fs"));
+const rl = Promise.promisifyAll(require("readline"));
+const Table = require("cli-table");
 
-const Trie = require('./tries');
-const i = rl.createInterface({input: process.stdin, output: process.stdout, terminal: true, prompt: 'News Article >'});
-
+const Trie = require("./tries");
+const i = rl.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: true,
+  prompt: "News Article >"
+});
+let companies = [];
 const trie = new Trie();
 
-let newsArticle = '';
+let newsArticle = "";
 
-fs.readFileAsync('companies.dat', 'utf-8').then(data => {
-	const companies = data.split('\n');
-	const companiesAndNicknames = companies.map((company) => {
-		return company.split('\t');
-	})
+fs.readFileAsync("companies.dat", "utf-8").then(data => {
+  const companies = data.split("\n");
+  const companiesAndNicknames = companies.map(company => {
+    return company.split("\t");
+  });
 
-	companiesAndNicknames.map((pseudonyms, id) => {
-		pseudonyms.map(pseudonym => {
-			trie.add(pseudonym, id);
-		})
-	});
+  companiesAndNicknames.map((pseudonyms, id) => {
+    pseudonyms.map(pseudonym => {
+      trie.add(pseudonym, id);
+    });
+  });
 
-	i.prompt();
-	i.on('line', line => {
-		if (line === '.') {
-			// analysis
-			console.log('news article:', newsArticle);
-			// const words = newsArticle.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
-			end();
-		} else {
-			newsArticle += line;
-			i.prompt();
-		}
-	})
+  i.prompt();
+  i.on("line", line => {
+    if (line === ".") {
+      // analysis
+      //console.log("news article:", newsArticle);
+      let hits = trie.search(newsArticle);
+      let wordCount = calculateWordCount(newsArticle);
+      let stats = calcStats(hits, wordCount);
+      let whatever = table(stats, wordCount);
+      end();
+    } else {
+      newsArticle += line;
+      i.prompt();
+    }
+  });
 });
 
 function end() {
-	console.log('exiting...');
-	i.close();
-	process.stdin.destroy();
+  console.log("exiting...");
+  i.close();
+  process.stdin.destroy();
 }
 
+
+//Function to calculate word count
+function calculateWordCount(article) {
+  if (article) {
+    article = article.replace(/(and|the|but|an|or|a)/g, "");
+    article = article.replace(
+      /(~|`|!|@|#|$|%|^|&|\*|\(|\)|{|}|\[|\]|;|:|\"|'|<|,|\.|>|\?|\/|\\|\||-|_|\+|=)/g,
+      ""
+    );
+    article = article.split(" ");
+    article = article.filter(word => word != "");
+    return article.length;
+  }
+}
+
+//function to calculate important statistics
+function calcStats(successfuls, words) {
+  let stats = {}; //object containing stats of each company
+  let arr = []; //array to hold all companies stats
+  let hitCount = 0; //hit count per company name
+  let number = 0; //temp number variable -dont worry about this 
+  let percentage = ""; //percentage template string thingy
+
+  //traverse through the object of successful tries
+  //get our company name, hit count, calculate percentage
+  for (let company in successfuls) {
+    if (successfuls.hasOwnProperty(company)) {
+      hitCount = successfuls[company];
+      number = (hitCount / words) * 100;
+      percentage = `${number}%`;
+      stats[company] = percentage;
+      stats["hit count"] = hitCount;
+      arr.push(stats);
+    }
+  }
+  return arr;
+}
+
+//function to turn our values and format it into a nice table format
+function table(arr, count) {
+  //create headings for table professor wants
+  const statsTable = new Table({
+    head: ["Company", "Hitcount", "Relevance"],
+    colWidths: [30, 30, 30]
+  });
+
+  //table for total hits and percentage
+  const totalStats = new Table({
+    colWidths: [30, 30, 30]
+  });
+
+  //table for total words in article
+  const wordCountTable = new Table({
+    colWidths: [45, 46]
+  });
+
+  let company;
+
+  //figure out how to get company names, hitcount, and relevance from array of objects
+  //then populate the table dynamically
+  for (let item in arr) {
+    console.log(arr[item]); // Will display contents of the object inside the array
+  }
+
+  //do total stats in a way where we can get calculations of total hits and percentage of total hits vs. total words
+  totalStats.push(["Total", 12, "10%"]);
+  wordCountTable.push(["Total Words:", count]);
+
+  //   console.log(statsTable.toString());
+  console.log(totalStats.toString());
+  console.log(wordCountTable.toString());
+}
 /*
 	Bibliography:
 	https://medium.com/@alexanderv/tries-javascript-simple-implementation-e2a4e54e4330
--- tries.js
+++ tries.js
@@ -1,75 +1,79 @@
 function Trie() {
-	this.head = {
-		key: '',
-		children: {}
-	}
+  this.head = {
+    key: "",
+    children: {}
+  };
 }
 
 /**
  * Function to add a new word to the Trie
  */
 Trie.prototype.add = function(key, companyId) {
+  // Remove all spaces in word when adding
+  key = key.trim().replace(/ /g, "");
 
-	// Remove all spaces in word when adding
-	key = key.trim().replace(/ /g, '');
+  // curNode will be used to traverse trie;
+  // newNode is the new node that will be created (if needed)
+  // curChar is the value of the character we need to check against when traversing
 
-	// curNode will be used to traverse trie;
-	// newNode is the new node that will be created (if needed)
-	// curChar is the value of the character we need to check against when traversing
+  let curNode = this.head;
+  let newNode = null;
 
-	let curNode = this.head;
-	let newNode = null;
+  let curChar = key.slice(0, 1);
+  key = key.slice(1);
 
-	let curChar = key.slice(0, 1);
-	key = key.slice(1);
+  // While the curNode has children that includes the curChar, and as long as the curChar exists, advance everything
+  while (curNode.children[curChar] !== undefined && curChar.length > 0) {
+    curNode = curNode.children[curChar];
+    curChar = key.slice(0, 1);
+    key = key.slice(1);
+  }
 
-	// While the curNode has children that includes the curChar, and as long as the curChar exists, advance everything
-	while (curNode.children[curChar] !== undefined && curChar.length > 0) {
-		curNode = curNode.children[curChar];
-		curChar = key.slice(0, 1);
-		key = key.slice(1);
-	}
+  // If, at this point, the char length is 0, this whole string is already in the trie, so update endOfWord to true
+  if (curChar.length === 0) {
+    curNode.endOfWord = true;
+  }
 
-	// If, at this point, the char length is 0, this whole string is already in the trie, so update endOfWord to true
-	if (curChar.length === 0) {
-		curNode.endOfWord = true;
-	}
+  // If we reached the end of Trie, create new nodes to extend Trie until all characters are added
+  while (curChar.length > 0) {
+    newNode = {
+      key: curChar,
+      companyId,
+      endOfWord: key.length === 0,
+      children: {}
+    };
 
-	// If we reached the end of Trie, create new nodes to extend Trie until all characters are added
-	while (curChar.length > 0) {
-		newNode = {
-			key: curChar,
-			companyId,
-			endOfWord: key.length === 0,
-			children: {}
-		}
+    curNode.children[curChar] = newNode;
+    curNode = newNode;
 
-		curNode.children[curChar] = newNode;
-		curNode = newNode;
-
-		curChar = key.slice(0, 1);
-		key = key.slice(1);
-	}
-}
+    curChar = key.slice(0, 1);
+    key = key.slice(1);
+  }
+};
 
 /**
  * Search for a word in the Tries
  */
 Trie.prototype.search = function(key) {
-	let curNode = this.head;
-	let curChar = key.slice(0, 1); // ''
+  let successfulTries = {};
+  let word = key;
+  let curNode = this.head;
+  let curChar = key.slice(0, 1); // ''
 
-	key = key.slice(1); // ''
+  key = key.slice(1); // ''
 
-	while (curNode.children[curChar] !== undefined && curChar.length > 0) {
-		curNode = curNode.children[curChar];
-		curChar = key.slice(0, 1);
-		key = key.slice(1);
-	}
+  while (curNode.children[curChar] !== undefined && curChar.length > 0) {
+    curNode = curNode.children[curChar];
+    curChar = key.slice(0, 1);
+    key = key.slice(1);
+  }
 
-	// Return TRUE if we are the end of the string (i.e. all the characters were found in the Trie tree)
-	// AND if this is the end of the word
-	return curChar.length === 0 && curNode.endOfWord;
-}
+  // Return TRUE if we are the end of the string (i.e. all the characters were found in the Trie tree)
+  // AND if this is the end of the word
+  if (curChar.length === 0 && curNode.endOfWord) {
+    successfulTries[word] = 1;
+    return successfulTries;
+  }
+};
 
 module.exports = Trie;
